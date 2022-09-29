eabb7b7f7cff56533e74b0f737f07ae34303d513
SHREY2527 <shreysoni009@gmail.com>
2022-09-29 19:12:18 +0530

    worked for admin panel to fetch data from base code

--- project/user/models.py
+++ project/user/models.py
@@ -1,14 +1,18 @@
 from pickle import TRUE
+from .views import Login,register
+from django.contrib.auth.models import User
+
 
 from django.db import models
 
 class operation_theater(models.Model):
-    username = models.CharField(max_length=200,null=True)
-    date = models.DateField(auto_now_add=True,null=True)
-    time = models.TimeField(auto_now_add=True,null=True)
+    Username = models.CharField(max_length=200,null=True)
+    email =models.EmailField(max_length=200,null=True)
+    date = models.DateField(max_length=50,null=True)
+    time = models.TimeField(max_length=50,null=True)
     room = models.CharField(max_length=30,null=True)
 
     def __str__(self):
-        return self.username
+        return self.Username
 
 
